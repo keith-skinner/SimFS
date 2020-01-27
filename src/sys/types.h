8c5b349f02bdaae4308f46abb74b2b3d4283a36e
Keith Skinner <keithskinner93@gmail.com>
2020-01-26 19:17:09 -0800

    added some more missing features on windows.

--- src/sys/types.h
+++ src/sys/types.h
@@ -2,4 +2,12 @@
 //A practice file.
 //typedef unsigned int mode_t;
 typedef unsigned int uid_t;
+typedef unsigned int gid_t;
 
+static uid_t getuid() {
+    return 7;
+}
+
+static gid_t getgid() {
+    return 5;
+}
